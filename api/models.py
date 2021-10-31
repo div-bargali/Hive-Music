d15406fc2646e4aca0e6d44f6cbda19fc7213c57
Divyanshu <divyanshubargali@gmail.com>
2021-10-31 15:44:30 +0530

    handle post request

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import string
 import random
 
-def generate_unique_codes():
+def generate_unique_code():
     length = 6
     """
     generates a random code of given length and returns it if not used before
@@ -15,7 +15,7 @@ def generate_unique_codes():
     return code
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(blank=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
